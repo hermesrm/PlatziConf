233cf164369984bd681d4a2a090eb602c394bc73
Hermes Rodriguez <hermesrodmo@gmail.com>
2021-05-02 17:34:14 +0200

    New files created:
    SplashScreenActivity.kt
    activity_splash_screen.xml
    
    Modified files:
    
    AndroidManifest.xml
    gradle.xml

---
+++ app/src/main/java/com/platzi/conf/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.platzi.conf.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platzi.conf.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
