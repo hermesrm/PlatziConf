a74fb3e2425ca9c83f5d1e3121a4fd6e935e987b
Hermes Rodriguez <hermesrodmo@gmail.com>
2021-05-04 20:58:40 +0200

    Firebase
    The project has been connected with Firebase.
    The google-services.json file has been added in the project files.
    Has been created the Conference.kt, Speaker.kt and Ubication.kt (objects) files.

---
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.platzi.conf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitud = ""
+    val longitud = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
